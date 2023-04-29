c760c42e5076a74404ec288614ae9e483f47929c
Muhammad Ahmed <119342876+MuhammadAhmed-Developer@users.noreply.github.com>
2023-04-29 20:13:47 +0100

    add upload page

--- src/pages/Frontend/Upload/Upload.jsx
+++ src/pages/Frontend/Upload/Upload.jsx
@@ -1,25 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react';
+import uploadimg from '../../../accests/images/upload image.png'
+
+const initialState = {
+   img:'',
+   msg:'',
+   imgName:''
+}
 
 export default function Upload() {
 
+  const [state, setstate] = useState(initialState)
+  const [file, setFile] = useState({})
 
-  const handleUpload = () => {
-    
+
+  const handleChange = (e) =>{
+     setstate(s=>({...s,[e.target.name]:e.target.value}))
   }
+
+  const handleSubmit = (e) => {
+       e.preventDefault()
+      console.log(state)
+      console.log(file)
+  }
+
 
   return (
     <>
-     <div className="container-fluid upload-pg">
-       <div className="row bg-warning mt-5 p-1">
-        <div className="col mt-5">
-          <p className='text-success fw-bold'><i class="bi bi-cone"></i> only upload media I own the rights to, and not any purchased or copied media.</p>
-          <p className='text-success fw-bold'><i class="bi bi-cone"></i> agree that my media will be released under the <a href='https://pixabay.com/service/terms/' target='_blank'>Content License.</a></p>
-        
-          <label htmlFor="img-file" className='btn btn-light btn-lg w-100' onClick={handleUpload}><i class="bi bi-cloud-arrow-up-fill text-success"></i> Upload images</label>
-          <input type="file" id='img-file' className='d-none'/>
+      <div className="container-fluid upload-pg">
+        <div className="row">
+          <div className="col">
+            <div className="card shadow border-0 p-3">
+              <form onSubmit={handleSubmit}>
+              <div className="col-12 text-center">
+                <label htmlFor="img-file" className='img-upload'><img src={uploadimg} alt="upload image" className='img-fluid w-50 rounded-circle' />
+                <br/> <span className='text-white' id='select'>
+                {file ? "Upload" : '{file.name}'}
+ 
+                </span>
+                </label>
+
+                <input type="file" id='img-file' className='d-none' name='img' 
+                onChange={e=>{setFile(e.target.files[0])}} />
+              </div>
+              <div className="row py-3">
+                <div className="col">
+                  <textarea name="msg" placeholder='Image Description' rows="5" className='form-control' onChange={handleChange}></textarea>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col" >
+                  <input type="text" className='form-control' value={file.name} placeholder='Image Name' name='imgName' onChange={handleChange} />
+                </div>
+              </div>
+              <div className="row py-3">
+                <div className="col" >
+                  <input type="checkbox" /> <span className='text-light ms-1'>Only upload media I own the rights to, and not any purchased or copied media.</span> <br/>
+                  <input type="checkbox" /> <span className='text-light  ms-1'> I agree that my media will be released under the 
+                  <a href="https://pixabay.com/service/terms/" target='_blank'> Content License.</a></span> 
+                </div>
+              </div>
+              <div className="row py-3">
+                <div className="col text-end" >
+                  <button className='btn btn-light w-50 fw-bold text-info'><i class="bi bi-cloud-arrow-up-fill"></i> Upload</button>
+                </div>
+              </div>
+              </form>
+            </div>
+          </div>
         </div>
-       </div>
-     </div>
+
+      </div>
     </>
   )
 }
