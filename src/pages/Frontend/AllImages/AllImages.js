c30e9c7118e384a114d2c84e5d804fa31e9d5895
Muhammad Ahmed <119342876+MuhammadAhmed-Developer@users.noreply.github.com>
2023-05-03 19:41:33 +0100

    ad ui

--- src/pages/Frontend/AllImages/AllImages.js
+++ src/pages/Frontend/AllImages/AllImages.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import img from '../../../accests/images/back-img.jpg'
-import pro from '../../../accests/images/facebook.png'
 
 export default function AllImages() {
   return (
@@ -9,12 +8,17 @@ export default function AllImages() {
      <section className='my-5 mx-3'>
         <div className="container-fluid my-5">
             <div className="row">
-                <div className="col-lg-4 col-md-6 cl-sm-12">
+                <div className="col-lg-4 col-md-6 cl-sm-12 box">
                    <img src={img} alt="" className='img-fluid '/>
-
-                    <div className='profiles'>
-                   <img src={pro} alt="" className='img-fluid mb-3 mt-2 rounded-circle' style={{width: 50}}  />
-                   <span className='fw-bold mt-3 text-white'>Muhammad Ahmed</span>
+                    <div className='icons m-3 text-end'>
+                    <i class="bi bi-chat-left-heart text-danger fs-4 me-4"></i><i class="bi bi-collection text-info fs-4"></i>
+                    </div>
+                    <div className='profiles m-3'>
+                    <i class="bi bi-person-circle text-warning fs-4 me-2"></i>
+                   <span className='fw-bold mt-3 text-white data'>Muhammad Ahmed</span>
+                   <span className='fw-bold mt-3 text-white data'>Muhammad Ahmed</span>
+                   <span className='fw-bold mt-3 text-white data'>Muhammad Ahmed</span>
+                   <span className='fw-bold mt-3 text-white data'>Muhammad Ahmed</span>
                     </div>
                 </div>
             </div>
